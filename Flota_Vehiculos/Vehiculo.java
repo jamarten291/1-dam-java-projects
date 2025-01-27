114cc61c2708d7583294b5d30c582fa4e95ea082
Jackson <146338884+ZeikeiZL@users.noreply.github.com>
2025-01-27 18:54:18 +0100

    Update Vehiculo.java
    removed things

--- Flota_Vehiculos/Vehiculo.java
+++ Flota_Vehiculos/Vehiculo.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FlotaVehiculos;
-
-/**
- *
- * @author Usuario
- */
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
