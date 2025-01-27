62d7bc651b1664d4f4aa4ba976516ae125a1bab9
Jackson <146338884+ZeikeiZL@users.noreply.github.com>
2025-01-27 18:52:23 +0100

    Update Furgoneta.java
    removed things

--- Flota_Vehiculos/Furgoneta.java
+++ Flota_Vehiculos/Furgoneta.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FlotaVehiculos;
 
-/**
- *
- * @author Usuario
- */
 public class Furgoneta extends Vehiculo {
 
     private int numPlazas;
