02f2579c54fb8667ed7a9a11441efd59d0fd9255
Jackson <146338884+ZeikeiZL@users.noreply.github.com>
2025-01-27 18:52:56 +0100

    Update Programa.java
    removed things

--- Flota_Vehiculos/Programa.java
+++ Flota_Vehiculos/Programa.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package FlotaVehiculos;
 
 import java.util.Objects;
 import java.util.Scanner;
 
-/**
- *
- * @author Usuario
- */
 public class Programa {
 
     public static int contCoches = 0;
